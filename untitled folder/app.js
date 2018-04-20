36e2814eff934c2aa2916ec4e02ac831905ea9f3
illy94 <kim2411@purdue.edu>
2018-04-20 13:03:20 -0400

    db3

--- untitled folder/app.js
+++ untitled folder/app.js
@@ -8,10 +8,31 @@ const config = {
   firebase.initializeApp(config);
 
   //get elements
-  const preObject = document.getElementById('object');
+  const preObject = document.getElementById('image');
+  const imageList = document.getElementById('dog');
+  const ulList  = document.getElementById('list');
 
-  const dbRefObject = firebase.database().ref().child('object');
+  const dbRefObject = firebase.database().ref().child('image');
+  const dbRefODog = dbRefObject.child('dog');
+  const dbRefOList = dbRefObject.child('something');
 
   //sync preObject
-  dbRefObject.on('value', snap => console.log(snap.val()));
+  dbRefObject.on('value', snap => {
+    console.log(snap.val() );
+  });
+
+  dbRefODog.on('child_added', snap => console.log(snap.val()));
+
+  /*dbRefODog .on('child_added', snap => {
+    const lx = document.createElement('lx');
+    lx.innerText = snap.val();
+    imageList.appendChild(lx);
+  })*/
+
+  /*dbRefList.on('child_added', snap => {
+  const li = document.createElement('li');
+  li.innerText = snap.val();
+  ulList.appendChild(li);
+});*/
+
 }());
