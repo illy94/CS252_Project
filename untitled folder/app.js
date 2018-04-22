3de4806e26fc7855e1c5f43bd3535f3d0b03fa2d
illy94 <kim2411@purdue.edu>
2018-04-22 14:35:18 -0400

    db 0421

--- untitled folder/app.js
+++ untitled folder/app.js
@@ -1,46 +1,63 @@
-(function(){
-const config = {
+(function() {
+  const config = {
   apiKey: "AIzaSyDWgofB00cK2UVeEN8EPf3ltTCPlrEMBKw",
   authDomain: "cs252-ar.firebaseapp.com",
   databaseURL: "https://cs252-ar.firebaseio.com",
   storageBucket: "cs252-ar.appspot.com",
 };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  //get elements
-  const preObject = document.getElementById('image');
+//Get Elements
+const preObject = document.getElementById('image');
+const ulList = document.getElementById('list');
 
-  const dbRefObject = firebase.database().ref().child('image');
-  //sync preObject
-  dbRefObject.on('value', snap => {
-    console.log(snap.val() );
-  });
-  //var Object = firebase.database().ref().child('image');
+//Create references
+const dbRefObject = firebase.database().ref().child('image');
+//const dbRefList = dbRefObject.child('hobbies');
 
-  var userRef = firebase.database().ref().child('image');
+//sync object changes
+dbRefObject.on('value', snap => {
+  preObject.innerText = JSON.stringify(snap.val(), null, 3);
+});
 
-  userRef.set ({
-   Dog: {
-      imageURL: "https://raw.githubusercontent.com/illy94/CS252_Project/master/myapptest/3dmodels/gltf/0/scene.gltf"
-   },
- });
-
-userRef.push ({
-   Cat: {
-      imageURL: "https://raw.githubusercontent.com/illy94/CS252_Project/master/myapptest/3dmodels/gltf/0/scene.gltf"
-   },
- });
-
-  /*dbRefODog .on('child_added', snap => {
-    const lx = document.createElement('lx');
-    lx.innerText = snap.val();
-    imageList.appendChild(lx);
-  })*/
-
-  /*dbRefList.on('child_added', snap => {
+dbRefObject.on('child_added', snap => {
   const li = document.createElement('li');
   li.innerText = snap.val();
+  li.id = snap.key;
   ulList.appendChild(li);
-});*/
 
+});
+
+dbRefObject.on('child_changed', snap => {
+  const liChanged = document.getElementById(snap.key);
+  liChanged.innerText = snap.val();
+
+});
+
+dbRefObject.on('child_removed', snap => {
+  const liRemove = document.getElementById(snap.key);
+  liRemove.innerText = snap.val();
+
+});
+
+writeNewPost("fox", "blah", "blah")
 }());
+
+function writeNewPost(name, imageurl, tdurl) {
+  // A post entry.
+  var postData = {
+    iname: name,
+    iurl: imageurl,
+    turl: tdurl
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('image').push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/image/' + newPostKey] = postData;
+  updates['/user-image/' + name + '/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
+}
